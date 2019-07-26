ba15c12bac1183e658944a120e95988b89c7ad34
Earl Cameron <mr.e.cameron@gmail.com>
2019-07-25 22:59:22 -0400

    file cleanup and basic route complete

---
+++ src/views/about.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+class About extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        console.log(`runs`)
+        return ( 
+            <div>
+                about me
+            </div>
+         );
+    }
+}
+ 
+export default About;
