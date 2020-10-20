184edad40373bf9578bbd155ba4e850b1c5020b7
Earl Cameron <mr.e.cameron@gmail.com>
2020-10-20 00:24:51 -0400

    Updates

---
+++ precache-manifest.1bcf5bccc14160dbf4adce3d2df09f9e.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9912c226b09ad10d60cf3d34d0bba8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdb3df2d79b9efdf8ef3",
+    "url": "/static/css/2.359d39dc.chunk.css"
+  },
+  {
+    "revision": "97bd534fb673a012cc79",
+    "url": "/static/css/main.167bb082.chunk.css"
+  },
+  {
+    "revision": "bdb3df2d79b9efdf8ef3",
+    "url": "/static/js/2.eea5651e.chunk.js"
+  },
+  {
+    "revision": "a0fa9e77153de75403f2f6d2293a1387",
+    "url": "/static/js/2.eea5651e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f74a2a3db365566631cf",
+    "url": "/static/js/3.cf30ced3.chunk.js"
+  },
+  {
+    "revision": "06a2977a80dd5598a081",
+    "url": "/static/js/4.f50ab05b.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/static/js/4.f50ab05b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ccc8b9130ccf55037ff",
+    "url": "/static/js/5.637cb92b.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.637cb92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97bd534fb673a012cc79",
+    "url": "/static/js/main.484d033f.chunk.js"
+  },
+  {
+    "revision": "7acb99cd5f1640a0b621",
+    "url": "/static/js/runtime-main.777550ce.js"
+  },
+  {
+    "revision": "9d72613072780b6f5d756ece8a073d22",
+    "url": "/static/media/apache.9d726130.png"
+  },
+  {
+    "revision": "e2cbe010f9b4e86b82e9977e89dc5d98",
+    "url": "/static/media/bob.e2cbe010.mp3"
+  },
+  {
+    "revision": "9dc9362dc8a304f18fa5550badf25a12",
+    "url": "/static/media/bootstrap4.9dc9362d.png"
+  },
+  {
+    "revision": "639149140d1b4bb9e8ea953bf78e4e15",
+    "url": "/static/media/crownvic.63914914.png"
+  },
+  {
+    "revision": "0243033c260f8753a284f020e44375fc",
+    "url": "/static/media/ec.0243033c.png"
+  },
+  {
+    "revision": "9877d335a0c277a205a1396c365f7f07",
+    "url": "/static/media/ec_noring.9877d335.png"
+  },
+  {
+    "revision": "3e4ce46abe0868f0a2d8f09f92f8a0f5",
+    "url": "/static/media/express.3e4ce46a.png"
+  },
+  {
+    "revision": "e224e7ef23d4a6c405ec17b63da2a206",
+    "url": "/static/media/gcloud.e224e7ef.png"
+  },
+  {
+    "revision": "ba93d14d94455bd6ee785d699f89b6c1",
+    "url": "/static/media/http.ba93d14d.png"
+  },
+  {
+    "revision": "182e7cd1e5a522f455524905fa7c0ae3",
+    "url": "/static/media/jamaican.182e7cd1.jpg"
+  },
+  {
+    "revision": "0fc916e59afb290e3c9c022d212cb2ca",
+    "url": "/static/media/java8.0fc916e5.png"
+  },
+  {
+    "revision": "b918be0750c8eb1dabbaea45fc329791",
+    "url": "/static/media/jsgame15.b918be07.webp"
+  },
+  {
+    "revision": "fff0f51f39aed1bc125a3355e3b40171",
+    "url": "/static/media/jsgame60.fff0f51f.webp"
+  },
+  {
+    "revision": "4a2f008cf503892b38cd22696bb6e231",
+    "url": "/static/media/linux.4a2f008c.png"
+  },
+  {
+    "revision": "2c1d4adfc3327203325bf88ed5e06933",
+    "url": "/static/media/mdchem.2c1d4adf.png"
+  },
+  {
+    "revision": "f693012a3450fcfeaba76ab803bd1f4e",
+    "url": "/static/media/mdchemmern.f693012a.png"
+  },
+  {
+    "revision": "3f86caad632f301277d1d25f56160f99",
+    "url": "/static/media/me1.3f86caad.jpg"
+  },
+  {
+    "revision": "00cfa4495c509436c5a54543c6038a51",
+    "url": "/static/media/mongodb.00cfa449.png"
+  },
+  {
+    "revision": "49dc99ba2192597a17f08a94f4d37a40",
+    "url": "/static/media/py3.49dc99ba.png"
+  },
+  {
+    "revision": "b515fdb10e139780707cd7776ed1f3c3",
+    "url": "/static/media/react.b515fdb1.png"
+  },
+  {
+    "revision": "2557dc24fd83b1754b40b75f5991f4d8",
+    "url": "/static/media/rest.2557dc24.png"
+  },
+  {
+    "revision": "94938a7cff091f8ca7db12d6a8c35aa7",
+    "url": "/static/media/ubuntu.94938a7c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7589ad9a2baa3fa877192814414aebd0.js"
+  "/precache-manifest.1bcf5bccc14160dbf4adce3d2df09f9e.js"
 );
 
 self.addEventListener('message', (event) => {
